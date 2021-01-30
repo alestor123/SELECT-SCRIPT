a7f4db141da59ee30d5766a50b49ec9c4a07b481
alestor123 <alestoraldous@gmail.com>
2021-01-30 16:14:37 +0530

    working

---
+++ App.js
@@ -0,0 +1,8 @@
+var {execSync} = require('child_process'),
+fs = require('fs'),
+scripts = [];
+module.exports = () => {
+var data = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+for(var sr in data.scripts) scripts.push([sr,'npm run '+sr])
+return scripts
+}
