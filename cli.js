47e8dbfedc5af2351102ed9c233e5dc91f12aa23
alestor123 <alestoraldous@gmail.com>
2021-01-31 13:42:39 +0530

    event

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var run = require('./App'),
-{execSync} = require('child_process')
+{ spawn } = require('child_process')
 select = require('cli-select'),
 chalk = require('chalk');
 (async () => {
@@ -8,5 +8,5 @@ var opts = [],
 data = run();
 data.map(src => opts.push(src[0]))
 var ans = await select({values: opts});
-console.log(execSync(data[ans.id][1]).toString())
+spawn('npm', ['run', data[ans.id][0]]).stdout.on('data',data => console.log(data.toString()))
 })();
